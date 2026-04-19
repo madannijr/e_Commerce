ffda40f23441d7ea5a0d878dba8bbae7b44c19af
Elhadj Abassy Diallo <elhadj-abassy.diallo@etu.u-bordeaux.fr>
2026-04-19 18:37:48 +0000

    Modification du fichier sidebar pour la deconnection

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -10,15 +10,15 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- SIDEBAR -->
-    <div class="sidebar">
-        <h3>Admin</h3>
-        <a href="index.php">🏠 Tableau de bord</a>
-        <a href="produits.php">📦 Produits</a>
-        <a href="commandes.php">🧾 Commandes</a>
-        <a href="clients.php">👤 Clients</a>
-        <a href="../index.php">🌍 Retour au site</a>
-        <a href="logout.php" class="logout">🚪 Déconnexion</a>
-    </div>
+        <!-- SIDEBAR -->
+        <div class="sidebar">
+            <h3>Admin</h3>
+            <a href="index.php">🏠 Tableau de bord</a>
+            <a href="produits.php">📦 Produits</a>
+            <a href="commandes.php">🧾 Commandes</a>
+            <a href="clients.php">👤 Clients</a>
+            <a href="../index.php">🌍 Retour au site</a>
+            <a href="../logout.php" class="logout">🚪 Déconnexion</a>
+        </div>
 </body>
 </html>
